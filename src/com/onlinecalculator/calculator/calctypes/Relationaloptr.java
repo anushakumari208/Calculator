dee5d1706962d7c087b4819587a7464ab26510ac
istar <istar@192.168.100.10>
2017-05-25 10:17:25 +0530

    relational operator

---
+++ src/com/onlinecalculator/calculator/calctypes/Relationaloptr.java
@@ -0,0 +1,23 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class Relationaloptr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		a=sc.nextInt();
+		System.out.println("enter b number");
+		b=sc.nextInt();
+		System.out.println("a<b"+(a<b));
+		System.out.println("a>b"+(a>b));
+		System.out.println("a==b"+(a==b));
+		System.out.println("a!=b"+(a!=b));
+		
+}
+}
+
+	
+
+
