c820a47028bc7814d45dcc2668b102dcae4f1546
istar <istar@192.168.100.10>
2017-05-25 10:45:32 +0530

    bitwise operator

---
+++ src/com/onlinecalculator/calculator/calctypes/bitwiseoptr.java
@@ -0,0 +1,19 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class bitwiseoptr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number");
+		a=sc.nextInt();
+		System.out.println("enter b number");
+		b=sc.nextInt();
+		System.out.println("a&b"+(a&b));
+		System.out.println("a|b"+(a|b));
+		System.out.println("a^b"+(a^b));
+		
+	}
+
+}
